c0e364484de792271b5b56748a175627f5d9dd7c
moshiur26 <moshiurrahmanb11@gmail.com>
2020-11-16 16:22:19 +0600

    basic server side rendering

---
+++ README.md
@@ -0,0 +1,1 @@
+"# mern-basic-boilerplate-simple-setup" 
---
+++ client/user/Users.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react'
+
+export default function Users() {
+    return (
+        <div>
+            <h1>Hello all users......!!</h1>
+        </div>
+    )
+};
