ecf76eb0b0a338e3e48602d095a1114c05596d28
moshiur26 <moshiurrahmanb11@gmail.com>
2021-07-04 02:54:23 +0600

    .solve PrivateRoute issue

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
+// import PrivateRoute from './auth/PrivateRoute'
+import PrivateRoute from './auth/PrivateRoute'
 import Home from './core/Home'
 import Users from './user/Users'
 import Signup from './user/Signup'
@@ -12,7 +14,9 @@ const MainRouter = () => {
       <Menu />
       <Switch>
         <Route exact path="/" component={Home}/>
-        <Route path="/users" component={Users}/>
+        <PrivateRoute path="/users" component={Users}/>
+
+        {/* <Route path="/users" component={Users}/> */}
         <Route path="/signup" component={Signup}/>
         <Route path="/signin" component={Signin}/>
         <Route path="/user/edit/:userId" component={EditProfile}/>
